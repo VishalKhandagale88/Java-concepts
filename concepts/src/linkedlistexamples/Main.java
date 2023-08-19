1fdfe0e7da984ccbc139b8b5d56748a69c0d52cf
vishal <vishal88kh@gmail.com>
2023-08-20 01:20:12 +0530

    added more elements in LinkedList

--- concepts/src/linkedlistexamples/Main.java
+++ concepts/src/linkedlistexamples/Main.java
@@ -5,7 +5,16 @@ public class Main {
         LinkedList linkedList = new LinkedList();
         linkedList.insert(2);
         linkedList.insert(3);
+        linkedList.insert(4);
         linkedList.insert(5);
+        linkedList.insert(6);
+        linkedList.insert(7);
+        linkedList.insert(8);
+        linkedList.insert(9);
+        linkedList.insert(10);
+
+
+
 
         linkedList.display();
     }
