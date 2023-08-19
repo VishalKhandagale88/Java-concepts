cf4bfe058e06ca9ca83326b4fae0355cf1e5515f
vishal <vishal88kh@gmail.com>
2023-08-20 00:08:03 +0530

    added code in main method

--- concepts/src/Main.java
+++ concepts/src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         LinkedList linkedList = new LinkedList();
+        linkedList.insert(2);
+        linkedList.insert(3);
+        linkedList.insert(5);
 
+        linkedList.display();
     }
 
 }
