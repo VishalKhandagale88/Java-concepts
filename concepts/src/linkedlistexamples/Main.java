a9f5e68c33ef229355be6d0e296bcad7031f3100
vishal <vishal88kh@gmail.com>
2023-08-20 00:12:57 +0530

    resolved git warning

--- concepts/src/linkedlistexamples/Main.java
+++ concepts/src/linkedlistexamples/Main.java
@@ -1,7 +1,5 @@
 package linkedlistexamples;
 
-import linkedlistexamples.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
