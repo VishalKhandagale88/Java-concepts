0b3f7845d14cbf9b6985eae09c69ad94983ac21e
vishal <vishal88kh@gmail.com>
2023-08-20 00:03:02 +0530

    added method to insert a new node

--- concepts/src/linkedlistexamples/LinkedList.java
+++ concepts/src/linkedlistexamples/LinkedList.java
@@ -6,4 +6,19 @@ public class LinkedList {
     public LinkedList() {
         this.head = null;
     }
+
+    void  insert(int data){
+        Node newNode = new Node();
+        if (head==null){
+            head = newNode;
+        }else {
+            Node current = head;
+            while (current.next!=null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+    }
+
 }
