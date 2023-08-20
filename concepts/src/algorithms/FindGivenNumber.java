abb8a623102ed97a14451a21e0d5f53742f96550
vishal <vishal88kh@gmail.com>
2023-08-20 19:41:36 +0530

    added a method that will return the number of times the number occured in the list

--- concepts/src/algorithms/FindGivenNumber.java
+++ concepts/src/algorithms/FindGivenNumber.java
@@ -1,5 +1,17 @@
 package algorithms;
 
-public class FindGivenNumber {
+import javax.swing.*;
 
+public class FindGivenNumber {
+    int[] Array={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+    public int printTheRepeatingNumber(int[] num1 , int n){
+        int count=0;
+        for (int j : num1) {
+            if (j % n == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
