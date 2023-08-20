1e477c33ae47ae4e0bd9985e1bde7a141df3032d
vishal <vishal88kh@gmail.com>
2023-08-20 19:43:10 +0530

    added logic in main method

--- concepts/src/algorithms/FindGivenNumber.java
+++ concepts/src/algorithms/FindGivenNumber.java
@@ -3,7 +3,7 @@ package algorithms;
 import javax.swing.*;
 
 public class FindGivenNumber {
-    int[] Array={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
 
     public int printTheRepeatingNumber(int[] num1 , int n){
         int count=0;
--- concepts/src/algorithms/Main.java
+++ concepts/src/algorithms/Main.java
@@ -2,6 +2,9 @@ package algorithms;
 
 public class Main {
     public static void main(String[] args) {
+        FindGivenNumber findGivenNumber = new FindGivenNumber();
+        int[] Array={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        findGivenNumber.printTheRepeatingNumber(Array,3);
 
     }
 }
