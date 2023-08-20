36f2fe48123ad1b56a603a7dbd6eebf193fe6375
vishal <vishal88kh@gmail.com>
2023-08-20 19:55:03 +0530

    added logic in main method to get the max number from the given array

--- concepts/src/algorithms/Main.java
+++ concepts/src/algorithms/Main.java
@@ -6,6 +6,8 @@ public class Main {
         int[] Array={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         int result = findGivenNumber.printTheNumberDivisibleByGivenNumber(Array, 3);
         System.out.println(result);
+        int maxNumber = findGivenNumber.maxElement(Array);
+        System.out.println(maxNumber);
 
     }
 }
