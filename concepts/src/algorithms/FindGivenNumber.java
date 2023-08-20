a0ffeb4e408bc4808c66adfcef64048b8f9190bb
vishal <vishal88kh@gmail.com>
2023-08-20 19:46:54 +0530

    changed the name of the method

--- concepts/src/algorithms/FindGivenNumber.java
+++ concepts/src/algorithms/FindGivenNumber.java
@@ -1,11 +1,9 @@
 package algorithms;
 
-import javax.swing.*;
-
 public class FindGivenNumber {
 
 
-    public int printTheRepeatingNumber(int[] num1 , int n){
+    public int printTheNumberDivisibleByGivenNumber(int[] num1 , int n){
         int count=0;
         for (int j : num1) {
             if (j % n == 0) {
--- concepts/src/algorithms/Main.java
+++ concepts/src/algorithms/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         FindGivenNumber findGivenNumber = new FindGivenNumber();
         int[] Array={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        int result = findGivenNumber.printTheRepeatingNumber(Array, 3);
+        int result = findGivenNumber.printTheNumberDivisibleByGivenNumber(Array, 3);
         System.out.println(result);
 
     }
