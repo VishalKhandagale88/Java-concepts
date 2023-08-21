51f133e804d0df6960972bd28eae54cd59c51ff6
vishal <vishal88kh@gmail.com>
2023-08-21 19:56:39 +0530

    added condition

--- concepts/src/programs/RepeatingNumberInArray.java
+++ concepts/src/programs/RepeatingNumberInArray.java
@@ -2,5 +2,12 @@ package programs;
 
 public class RepeatingNumberInArray {
     /*Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9
+
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9
     * */
 }
