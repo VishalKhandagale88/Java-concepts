33ecb3600afa80358706169f458311fe26ee48f8
vishal <vishal88kh@gmail.com>
2023-08-22 17:27:35 +0530

    added logic

--- concepts/src/programs/RepeatingNumberInArray.java
+++ concepts/src/programs/RepeatingNumberInArray.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class RepeatingNumberInArray {
     /*
-    * Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array. Return the answer in ascending order. If no such element is found, return list containing [-1].
+    * Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array.
+    *  Return the answer in ascending order. If no such element is found, return list containing [-1].
 
       Note: The extra space is only for the array to be returned. Try and perform all operations within the provided array.
     *    a[] = {2,3,1,2,3}
@@ -26,11 +27,12 @@ public class RepeatingNumberInArray {
     // hint 1 : Try to use the given array as a hash table.
 
     /*
-    * 1- Traverse the given array from i= 0 to n-1 elements
-     Go to index arr[i]%n and increment its value by n.
+    *
+    1- Traverse the given array from i= 0 to n-1 elements
+        Go to index arr[i]%n and increment its value by n.
     2- Now traverse the array again and print all those
-    elements at indexes i for which arr[i]/n is greater
-    than 1.
+        elements at indexes i for which arr[i]/n is greater
+        than 1.
 
     This approach works because all elements are in range
     from 0 to n-1 and arr[i]/n would be greater than 1
