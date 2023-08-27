5edfbeb09728bd54eafec4beaa264e6d3ae99cbc
vishal <vishal88kh@gmail.com>
2023-08-28 01:42:50 +0530

    added linearSearch method in LinearSearch.java

--- concepts/src/arrays/LinearSearch.java
+++ concepts/src/arrays/LinearSearch.java
@@ -1,4 +1,14 @@
 package arrays;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        // In linear search basically we are given a number and we have to find it in an array
+
+    }
+
+    public void linearSearch(int[]array,int target){
+
+    }
+
+
 }
