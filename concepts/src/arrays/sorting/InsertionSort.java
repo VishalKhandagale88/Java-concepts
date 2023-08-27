cd80cfa99db35c238526c24ccf7ad8e9fcaefdd7
vishal <vishal88kh@gmail.com>
2023-08-28 02:43:12 +0530

    added logic of insertion sort

--- concepts/src/arrays/sorting/InsertionSort.java
+++ concepts/src/arrays/sorting/InsertionSort.java
@@ -5,6 +5,16 @@ public class InsertionSort {
 
     }
     public int[] insertionSort(int[] array){
+        int n = array.length;
+        for (int i=1;i<n;i++){
+            int key = array[i];
+            int j = i-1;
+            while (j>=0 && array[j]>key){
+                array[i+1] = key;
+                j--;
+            }
+            array[j+1] = key;
+        }
         return null;
     }
 }
