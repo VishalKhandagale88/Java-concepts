3d585419dde6af5e2621e736c3e332c8a3b67f80
vishal <vishal88kh@gmail.com>
2023-08-28 01:44:46 +0530

    declared array in main method and created a object of scanner class

--- concepts/src/arrays/LinearSearch.java
+++ concepts/src/arrays/LinearSearch.java
@@ -1,14 +1,21 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class LinearSearch {
     public static void main(String[] args) {
         // In linear search basically we are given a number and we have to find it in an array
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] array = new int[size];
+
 
     }
 
     public void linearSearch(int[]array,int target){
 
     }
+
 
 
 }
