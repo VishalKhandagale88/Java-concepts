b4ebadeff1b0020524334560fd685044cdc025ea
vishal <vishal88kh@gmail.com>
2023-08-28 02:10:30 +0530

    added log in main method to get input for an array

--- concepts/src/arrays/LinearSearch.java
+++ concepts/src/arrays/LinearSearch.java
@@ -6,13 +6,20 @@ public class LinearSearch {
     public static void main(String[] args) {
         // In linear search basically we are given a number and we have to find it in an array
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array ");
         int size = sc.nextInt();
         int[] array = new int[size];
-
-
+        for (int i=0;i<size;i++){
+            System.out.print("Enter value : ");
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Enter the target value");
+        int target = sc.nextInt();
+        LinearSearch.linearSearch(array,target);
     }
 
-    public void linearSearch(int[]array,int target){
+    public  static void linearSearch(int[]array,int target){
+        int n = array.length;
 
     }
 
