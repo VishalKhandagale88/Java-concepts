4c0f973f7c6db3f84e4d74051f9d41c8239a498f
vishal <vishal88kh@gmail.com>
2023-09-01 17:56:44 +0530

    added method that will give greatest sum of array

--- concepts/src/arrays/subarray/KadaneAlgorithm.java
+++ concepts/src/arrays/subarray/KadaneAlgorithm.java
@@ -3,7 +3,22 @@ package arrays.subarray;
 public class KadaneAlgorithm {
     public static void main(String[] args) {
 
+        int [] num = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSubArray = KadaneAlgorithm.maxSubArraySum(num);
+        System.out.println(maxSubArray);
     }
+    public static int maxSubArraySum(int[] num) {
+        int maxEndingHere = num[0];
+        int maxSoFar = num[0];
+
+        for (int i = 1; i < num.length; i++) {
+            maxEndingHere = Math.max(num[i], maxEndingHere + num[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
 
 
 }
