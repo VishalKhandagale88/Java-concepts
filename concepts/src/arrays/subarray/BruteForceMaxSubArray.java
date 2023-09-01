369862341e268941fefa97e34f588552067c7db6
vishal <vishal88kh@gmail.com>
2023-09-01 19:27:13 +0530

    added what is subarray

--- concepts/src/arrays/subarray/BruteForceMaxSubArray.java
+++ concepts/src/arrays/subarray/BruteForceMaxSubArray.java
@@ -21,3 +21,72 @@ public class BruteForceMaxSubArray {
         return maxSum;
     }
 }
+/*
+*
+* Certainly! Here are all the possible subarrays of the input array `[-2, 1, -3, 4, -1, 2, 1, -5, 4]`:
+
+1. Single-Element Subarrays:
+   - [-2]
+   - [1]
+   - [-3]
+   - [4]
+   - [-1]
+   - [2]
+   - [1]
+   - [-5]
+   - [4]
+
+2. Two-Element Subarrays:
+   - [-2, 1]
+   - [1, -3]
+   - [-3, 4]
+   - [4, -1]
+   - [-1, 2]
+   - [2, 1]
+   - [1, -5]
+   - [-5, 4]
+
+3. Three-Element Subarrays:
+   - [-2, 1, -3]
+   - [1, -3, 4]
+   - [-3, 4, -1]
+   - [4, -1, 2]
+   - [-1, 2, 1]
+   - [2, 1, -5]
+   - [1, -5, 4]
+
+4. Four-Element Subarrays:
+   - [-2, 1, -3, 4]
+   - [1, -3, 4, -1]
+   - [-3, 4, -1, 2]
+   - [4, -1, 2, 1]
+   - [-1, 2, 1, -5]
+   - [2, 1, -5, 4]
+
+5. Five-Element Subarrays:
+   - [-2, 1, -3, 4, -1]
+   - [1, -3, 4, -1, 2]
+   - [-3, 4, -1, 2, 1]
+   - [4, -1, 2, 1, -5]
+   - [-1, 2, 1, -5, 4]
+
+6. Six-Element Subarrays:
+   - [-2, 1, -3, 4, -1, 2]
+   - [1, -3, 4, -1, 2, 1]
+   - [-3, 4, -1, 2, 1, -5]
+   - [4, -1, 2, 1, -5, 4]
+
+7. Seven-Element Subarrays:
+   - [-2, 1, -3, 4, -1, 2, 1]
+   - [1, -3, 4, -1, 2, 1, -5]
+   - [-3, 4, -1, 2, 1, -5, 4]
+
+8. Eight-Element Subarrays:
+   - [-2, 1, -3, 4, -1, 2, 1, -5]
+   - [1, -3, 4, -1, 2, 1, -5, 4]
+
+9. Nine-Element Subarray:
+   - [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+These are all the possible subarrays of the given input array, including single-element, two-element, three-element, and so on, up to the full array itself. Subarrays can vary in length and contain different combinations of elements from the original array.
+* */
