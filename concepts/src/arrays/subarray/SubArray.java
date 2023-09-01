77868983aa643ab2a39efd3c3d98c73c20941327
vishal <vishal88kh@gmail.com>
2023-09-01 18:03:30 +0530

    added logic in formSubArray method

--- concepts/src/arrays/subarray/SubArray.java
+++ concepts/src/arrays/subarray/SubArray.java
@@ -5,7 +5,13 @@ public class SubArray {
 
     }
 
-    public static void formSubArray(int[] array , int startingIndex , int endingIndex){
+    public static int[] formSubArray(int[] array , int startingIndex , int endingIndex){
+        int n = array.length;
+        int[]  subArray = new int[endingIndex-startingIndex];
+        for (int i=startingIndex;i<endingIndex;i++){
+            subArray[i] = array[i];
+        }
+        return subArray;
 
     }
 
