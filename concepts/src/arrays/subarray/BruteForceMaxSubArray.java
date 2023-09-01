af18fab636f2de18aadd74feb1622905368b62d4
vishal <vishal88kh@gmail.com>
2023-09-01 18:37:40 +0530

    added logic to find the max sum form array

--- concepts/src/arrays/subarray/BruteForceMaxSubArray.java
+++ concepts/src/arrays/subarray/BruteForceMaxSubArray.java
@@ -2,11 +2,21 @@ package arrays.subarray;
 
 public class BruteForceMaxSubArray {
     public static void main(String[] args) {
-
+        int [] num = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = bruteforce(num);
+        System.out.println(maxSum);
     }
 
     public static int bruteforce(int[] arr){
-
-        return 1;
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+        int currentValue =0 ;
+        for (int i=0;i<n;i++){
+            for (int j=i;j<n;j++){
+                currentValue+=arr[i];
+                maxSum = Math.max(currentValue,maxSum);
+            }
+        }
+        return maxSum;
     }
 }
