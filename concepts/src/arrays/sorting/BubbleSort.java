06033679767ea4df4c12c0657e04dc8bee98f269
vishal <vishal88kh@gmail.com>
2023-09-02 21:28:13 +0530

    added logic to sort an array

--- concepts/src/arrays/sorting/BubbleSort.java
+++ concepts/src/arrays/sorting/BubbleSort.java
@@ -4,4 +4,22 @@ public class BubbleSort {
     public static void main(String[] args) {
 
     }
+
+    public static int[] bubbleSort(int[] arr){
+        int n = arr.length;
+        for (int i=0;i<n;i++){
+            boolean swapped = false;
+            for (int j=0;j<n-i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+        return  arr;
+    }
 }
