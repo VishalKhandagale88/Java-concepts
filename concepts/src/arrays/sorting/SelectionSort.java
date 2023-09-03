1f0c648bf235d7415e64da90bc6889a0694a0059
vishal <vishal88kh@gmail.com>
2023-09-03 17:14:26 +0530

    changed normal form loop to enhanced for loop

--- concepts/src/arrays/sorting/SelectionSort.java
+++ concepts/src/arrays/sorting/SelectionSort.java
@@ -2,13 +2,13 @@ package arrays.sorting;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = {5,6,3,4,6,10,7,1};
+        int[] arr = {5,6,3,4,2,10,7,1};
         int[] selectionSortArray = SelectionSort.selectionSort(arr);
         SelectionSort.printArray(selectionSortArray);
 
     }
 
-    public  static   int[] selectionSort(int arr[]){
+    public  static   int[] selectionSort(int[] arr){
         int n =  arr.length;
         for (int i =0;i<n-1;i++){
             int minIndex = i;
@@ -27,10 +27,10 @@ public class SelectionSort {
         return arr;
     }
 
-    public  static  void printArray(int arr[]){
-        int n = arr.length;
-        for (int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+    public  static  void printArray(int[] arr){
+
+        for (int i :arr){
+            System.out.print(i+" ");
         }
     }
 
