cc4611404c8c409f4ebf56afa9afe982a7c21070
vishal <vishal88kh@gmail.com>
2023-09-04 01:45:14 +0530

    added logic in mergeSort method

--- concepts/src/arrays/sorting/MergeSort.java
+++ concepts/src/arrays/sorting/MergeSort.java
@@ -4,4 +4,13 @@ public class MergeSort {
     public static void main(String[] args) {
 
     }
+
+    public  static  void  mergeSort(int[] arr){
+        int startingIndex = 0;
+        int endingIndex = arr.length-1;
+        if (startingIndex<endingIndex){
+            int middle = (startingIndex+endingIndex)/2;
+        }
+
+    }
 }
