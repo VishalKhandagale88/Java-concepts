53ff80cd58eab6f9620180e339ba259cdbfdae45
vishal <vishal88kh@gmail.com>
2023-09-07 01:03:00 +0530

    added MergeSort class

--- concepts/src/arrays/sorting/MergeSort.java
+++ concepts/src/arrays/sorting/MergeSort.java
@@ -1,19 +1,9 @@
 package arrays.sorting;
 
 public class MergeSort {
-    public static void main(String[] args) {
+    public static void mergeSort(int[] arr, int startingIndex, int endingIndex) {
 
     }
 
-    public  static  void  mergeSort(int[] arr){
-        int startingIndex = 0;
-        int endingIndex = arr.length-1;
-        if (startingIndex<endingIndex){
-            int middle = (startingIndex+endingIndex)/2;
-        }
 
-    }
-    public static void  merge(int[] arr,int startingIndex,int middle, int endingIndex){
-
-    }
 }
