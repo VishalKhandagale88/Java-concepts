d872551657d41660e220c3cbc2890c40bc86e631
vishal <vishal88kh@gmail.com>
2023-09-07 01:04:39 +0530

    added recursion in mergerSort() method

--- concepts/src/arrays/sorting/MergeSort.java
+++ concepts/src/arrays/sorting/MergeSort.java
@@ -2,7 +2,12 @@ package arrays.sorting;
 
 public class MergeSort {
     public static void mergeSort(int[] arr, int startingIndex, int endingIndex) {
+        if (startingIndex<endingIndex){
+            int middleIndex = (startingIndex+endingIndex)/2;
+            mergeSort(arr,startingIndex,middleIndex);
+            mergeSort(arr,middleIndex+1,endingIndex);
 
+        }
     }
 
     public static void main(String[] args) {
