c047194e4e7ea27864ce7806a05c4da30182333e
vishal <vishal88kh@gmail.com>
2025-01-06 22:05:02 +0530

    Added main

---
+++ concepts/src/linkedlist/singlylinkedlist/withtail/examples/orderqueue/Main.java
@@ -0,0 +1,15 @@
+package linkedlist.singlylinkedlist.withtail.examples.orderqueue;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderQueue orderQueueLinkedList = new OrderQueue();
+        orderQueueLinkedList.add("data");
+        orderQueueLinkedList.add("Hello");
+        orderQueueLinkedList.add("1212");
+        orderQueueLinkedList.add("d2323");
+        orderQueueLinkedList.add("4343");
+        orderQueueLinkedList.add("Gill");
+        orderQueueLinkedList.add("NEO");
+        orderQueueLinkedList.display();
+    }
+}
