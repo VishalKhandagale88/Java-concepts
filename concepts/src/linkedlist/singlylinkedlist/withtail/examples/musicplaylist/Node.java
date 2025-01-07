5a161db4d37ab51ffd6e498a5f091068eddf75c7
vishal <vishal88kh@gmail.com>
2025-01-07 20:54:33 +0530

    Added music playlist node

---
+++ concepts/src/linkedlist/singlylinkedlist/withtail/examples/musicplaylist/Node.java
@@ -0,0 +1,10 @@
+package linkedlist.singlylinkedlist.withtail.examples.musicplaylist;
+
+public class Node {
+    String song;
+    Node next;
+    Node(String song){
+        this.song = song;
+        this.next = null;
+    }
+}
