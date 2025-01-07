c191d2a1dc1f17a98287a99a1d514c315d8aa9ca
vishal <vishal88kh@gmail.com>
2025-01-07 21:43:07 +0530

    Added Node

---
+++ concepts/src/linkedlist/singlylinkedlist/withouttail/operations/recursivelengthofliinkedlist/Node.java
@@ -0,0 +1,10 @@
+package linkedlist.singlylinkedlist.withouttail.operations.recursivelengthofliinkedlist;
+
+public class Node {
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
