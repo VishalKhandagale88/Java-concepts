24b991b0a7684e463e3c5ece335618f30df94945
vishal <vishal88kh@gmail.com>
2025-01-07 21:28:06 +0530

    Added Node

---
+++ concepts/src/linkedlist/singlylinkedlist/withouttail/operations/lengthofliinkedlist/Node.java
@@ -0,0 +1,11 @@
+package linkedlist.singlylinkedlist.withouttail.operations.lengthofliinkedlist;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next =null;
+    }
+}
