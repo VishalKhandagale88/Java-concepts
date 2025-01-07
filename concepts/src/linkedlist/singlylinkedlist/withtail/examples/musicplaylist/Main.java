475b61d212aa87f8417ea9b83cbf3a137e6bd0be
vishal <vishal88kh@gmail.com>
2025-01-07 20:59:25 +0530

    Added main

---
+++ concepts/src/linkedlist/singlylinkedlist/withtail/examples/musicplaylist/Main.java
@@ -0,0 +1,13 @@
+package linkedlist.singlylinkedlist.withtail.examples.musicplaylist;
+
+public class Main {
+    public static void main(String[] args) {
+        MusicPlayList musicPlayListLinkedList = new MusicPlayList();
+        musicPlayListLinkedList.add("Song1");
+        musicPlayListLinkedList.add("Song2");
+        musicPlayListLinkedList.add("Song3");
+        musicPlayListLinkedList.add("Song4");
+        musicPlayListLinkedList.add("Song5");
+        musicPlayListLinkedList.display();
+    }
+}
